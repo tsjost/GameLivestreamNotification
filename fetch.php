e0569b17969ed96409b897c383ba7dcc43af369d
tsjost <vcs@tsjo.st>
2021-03-28 16:16:48 +0200

    Display Twitch user's name, avatar & stream preview image in Discord message

--- fetch.php
+++ fetch.php
@@ -1,12 +1,30 @@
 <?php
 require __DIR__ .'/config.php';
 
-function send_discord_notification($message, $messageID = null)
+function send_discord_notification($message, $username = null, $avatar = null, $preview_image = null, $messageID = null)
 {
+	$data = [
+		'content' => $message,
+	];
+
+	if ($username) {
+		$data['username'] = $username;
+	}
+	if ($avatar) {
+		$data['avatar_url'] = $avatar;
+	}
+	if ($preview_image) {
+		$data['embeds'][] = [
+			'image' => [
+				'url' => str_replace('-{width}x{height}', '', $preview_image),
+			],
+		];
+	}
+
 	$c = curl_init('https://discord.com/api/webhooks/'. DISCORD_WEBHOOK_ID .'/'. DISCORD_WEBHOOK_TOKEN . ($messageID ? "/messages/$messageID" : '') .'?wait=true');
 	$o = [
 		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_POSTFIELDS => "content=$message",
+		CURLOPT_POSTFIELDS => 'payload_json='. json_encode($data),
 		CURLOPT_CUSTOMREQUEST => $messageID ? 'PATCH' : 'POST',
 	];
 	curl_setopt_array($c, $o);
@@ -15,6 +33,30 @@ function send_discord_notification($message, $messageID = null)
 
 	$json = json_decode($ret);
 	return $json->id;
+}
+
+function get_twitch_user_data(array $userIDs)
+{
+	if (empty($userIDs)) return [];
+
+	$querystring = '?id='. implode('&id=', $userIDs);
+	$url = 'https://api.twitch.tv/helix/users'. $querystring;
+	$c = curl_init($url);
+	$o = [
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_HTTPHEADER => [
+			'Authorization: Bearer '. TWITCH_OAUTH_SECRET,
+			'Client-Id: '. TWITCH_CLIENT_ID,
+		],
+	];
+	curl_setopt_array($c, $o);
+	$ret = curl_exec($c);
+	curl_close($c);
+
+	$data = json_decode($ret)->data;
+	$data = array_combine(array_map(fn($x) => $x->id, $data), $data);
+
+	return $data;
 }
 
 $url = 'https://api.twitch.tv/helix/streams?first=100&game_id='. TWITCH_GAME_ID;
@@ -47,15 +89,22 @@ if (file_exists($filename)) {
 	$prev_filedata = array_map(fn($x) => explode(' ', $x), $prev_filedata);
 }
 
+$userIDs = array_map(fn($x) => $x->user_id, $streams->data);
+$userdata = get_twitch_user_data($userIDs);
+
 $filedata = '';
 foreach ($streams->data as $stream) {
 	$id = $stream->id;
+	$user_id = $stream->user_id;
 	$game = $stream->game_name;
 	$userslug = $stream->user_login;
 	$username = $stream->user_name;
 	$stream_title = $stream->title;
 	$stream_type = $stream->type;
 	$viewers = $stream->viewer_count;
+	$thumbnail = $stream->thumbnail_url;
+
+	$profile_image = $userdata[$user_id]->profile_image_url ?? null;
 
 	$already_notified = isset($prev_filedata[$id]);
 	$max_viewers = $viewers;
@@ -69,7 +118,7 @@ foreach ($streams->data as $stream) {
 	echo "$username is $stream_type streaming **$game** for $viewers viewers! <https://twitch.tv/$userslug> (\"$stream_title\")\n";
 	if ( ! $already_notified or $max_viewers > $prev_viewers) {
 		echo "   notifying!\n";
-		$msgID = send_discord_notification(DISCORD_MESSAGE_PREFIX ."`$username` is $stream_type streaming **$game** for a peak of $max_viewers viewers! <https://twitch.tv/$userslug> (\"$stream_title\")", $msgID);
+		$msgID = send_discord_notification(DISCORD_MESSAGE_PREFIX ."`$username` is $stream_type streaming **$game** for a peak of $max_viewers viewers! <https://twitch.tv/$userslug> (\"$stream_title\")", $username, $profile_image, $thumbnail, $msgID);
 	}
 
 
