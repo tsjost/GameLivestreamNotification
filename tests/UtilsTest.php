6074d5d6c874495baa14a3acfa4821726d448ee9
tsjost <vcs@tsjo.st>
2023-02-02 20:58:31 +0100

    Test upsert_config_constants

---
+++ tests/UtilsTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+require __DIR__ .'/../vendor/autoload.php';
+require __DIR__ .'/../utils.php';
+
+use \PHPUnit\Framework\TestCase;
+
+const TESTCONFIG_FILE = __DIR__ . '/../testconfig.php';
+
+final class UtilsTest extends TestCase
+{
+	public function test_upsert_config_constants(): void
+	{
+		upsert_config_constants([
+			'HELLO'       => 'this is a greeting',
+			'HELLO_THERE' => 'say hello you must',
+			'FOO_BAR_BAZ' => 'lorem ipsum dolor sit amet',
+		], TESTCONFIG_FILE);
+
+		$expected = <<<EOF
+<?php
+
+const HELLO			= 'this is a greeting';
+const HELLO_THERE='say hello you must';
+
+const FOO_BAR_BAZ      =     "lorem ipsum dolor sit amet";
+EOF;
+		$this->assertSame($expected, file_get_contents(TESTCONFIG_FILE));
+	}
+
+	protected function setUp(): void
+	{
+		file_put_contents(TESTCONFIG_FILE, <<<EOF
+<?php
+
+const HELLO			= '';
+const HELLO_THERE='general kenobi';
+
+const FOO_BAR_BAZ      =     "stuff goes in here";
+EOF);
+	}
+
+	public static function tearDownAfterClass(): void
+	{
+		unlink(TESTCONFIG_FILE);
+	}
+}
--- utils.php
+++ utils.php
@@ -2,22 +2,30 @@
 
 const CONFIG_FILE = __DIR__ .'/config.php';
 
-function upsert_config_constants($keyvalues)
+// PHP <8.0 support
+if ( ! function_exists('str_contains')) {
+	function str_contains($haystack, $needle)
+	{
+		return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+	}
+}
+
+function upsert_config_constants(array $keyvalues, string $filename = CONFIG_FILE)
 {
-	$config = file_get_contents(CONFIG_FILE);
+	$config = file_get_contents($filename);
 	if ( ! $config) {
-		throw new \RuntimeException('Unable to read '. CONFIG_FILE);
+		throw new \RuntimeException('Unable to read '. $filename);
 	}
 
 	foreach ($keyvalues as $name => $value) {
 		if (str_contains($config, $name)) {
-			$config = preg_replace('/([\n^]const '. $name .'\s.*?[\'"])[0-9a-z]+([\'"])/', "\\1$value\\2", $config);
+			$config = preg_replace('/([\n^]const '. $name .'\s*=\s*[\'"])[0-9a-z\s]*([\'"])/', "\\1$value\\2", $config);
 		} else {
 			$config .= "\nconst $name = '$value';\n";
 		}
 	}
 
-	if (false === file_put_contents(CONFIG_FILE, $config)) {
-		throw new \RuntimeException('Unable to write to '. CONFIG_FILE);
+	if (false === file_put_contents($filename, $config)) {
+		throw new \RuntimeException('Unable to write to '. $filename);
 	}
 }
